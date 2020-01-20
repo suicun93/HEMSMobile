93e40dd4d0c88f3cbcdc9b0dfb5e4e65153bc319
suicun9x <duchoang191@gmail.com>
2020-01-20 17:37:47 +0900

    Add device activity

---
+++ app/src/main/java/AView/DeviceActivity.java
@@ -0,0 +1,1 @@
+package AView;import androidx.appcompat.app.AppCompatActivity;import android.os.Bundle;public class DeviceActivity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_device);    }}
