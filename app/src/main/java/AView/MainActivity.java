13f3fb3086272807d990d9baab2a881356efa134
suicun9x <duchoang191@gmail.com>
2020-01-22 17:23:42 +0900

    Reload data every 5 seconds in main activity
    Signed-off-by: suicun9x <duchoang191@gmail.com>

--- app/src/main/java/AView/MainActivity.java
+++ app/src/main/java/AView/MainActivity.java
@@ -3,6 +3,7 @@ package AView;
 import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -14,6 +15,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import Main.EchoController;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
       private RecyclerView rvDevices;
       private Button btnUpdate = null;
       private TextView txtItemCount = null;
+      // Save state
+      private Parcelable recyclerViewState;
 
       @Override
       protected void onCreate(Bundle savedInstanceState) {
@@ -37,43 +43,80 @@ public class MainActivity extends AppCompatActivity {
                   WifiManager.MulticastLock lock = wifi.createMulticastLock("HEMSmulticast");
                   lock.acquire();
             }
-            // </editor-fold>
-            // Start Controller and Update
-            try {
-                  EchoController.startController();
-            } catch (Exception e) {
-                  Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
 
             // Get View
             notFoundFace = findViewById(R.id.imageView);
             rvDevices = findViewById(R.id.listView);
             btnUpdate = findViewById(R.id.button);
             txtItemCount = findViewById(R.id.txtItemCount);
-            btnUpdate.setOnClickListener(v -> update());
+            // </editor-fold>
+            // Start Controller and Update
+            try {
+                  EchoController.startController();
+            } catch (Exception e) {
+                  Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+                  return;
+            }
+
+            // Button Update's onClick
+            btnUpdate.setOnClickListener(v -> {
+                  disableButtonUpdateInSeveralSeconds();
+                  update();
+            });
+
+            // Reload when scroll down
+            rvDevices.setOnFlingListener(new RecyclerView.OnFlingListener() {
+                  private static final int SWIPE_VELOCITY_THRESHOLD = 4000;
+
+                  @Override
+                  public boolean onFling(int velocityX, int velocityY) {
+                        if (btnUpdate.isEnabled()) {
+                              if (velocityY < (-1) * SWIPE_VELOCITY_THRESHOLD) {
+                                    disableButtonUpdateInSeveralSeconds();
+                                    update();
+                                    return true;
+                              }
+                        }
+                        return false;
+                  }
+            });
+
+            // Update View after 5 seconds
+            new Timer().schedule(new TimerTask() {
+                  @Override
+                  public void run() {
+                        runOnUiThread(() -> {
+                              if (rvDevices.getLayoutManager() != null) {
+                                    recyclerViewState = rvDevices.getLayoutManager().onSaveInstanceState();
+                              }
+                              update();
+                              if (recyclerViewState != null) {
+                                    rvDevices.getLayoutManager().onRestoreInstanceState(recyclerViewState);
+                              }
+                        });
+                  }
+            }, 1000, 5000);
       }
 
-      private void disableButtonUpdate() {
-            // Disable button update to wait for 1 second.
-            new Thread(() -> {
-                  runOnUiThread(() -> {
-                        btnUpdate.setText(R.string.updating);
-                        btnUpdate.setEnabled(false);
-                  });
-                  try {
-                        Thread.sleep(3000);
-                  } catch (InterruptedException e) {
-                        System.out.println("InterruptedException: " + e.getMessage());
+      private void disableButtonUpdateInSeveralSeconds() {
+            // Disable button update to wait for 2 second.
+            int delay = 2000;
+            runOnUiThread(() -> {
+                  btnUpdate.setText(R.string.updating);
+                  btnUpdate.setEnabled(false);
+            });
+            new Timer().schedule(new TimerTask() {
+                  @Override
+                  public void run() {
+                        runOnUiThread(() -> {
+                              btnUpdate.setText(R.string.update);
+                              btnUpdate.setEnabled(true);
+                        });
                   }
-                  runOnUiThread(() -> {
-                        btnUpdate.setText(R.string.update);
-                        btnUpdate.setEnabled(true);
-                  });
-            }).start();
+            }, delay);
       }
 
       public void update() {
-            disableButtonUpdate();
             // Reload list Devices.
             if (!EchoController.listDevice().isEmpty()) {
                   rvDevices.setVisibility(View.VISIBLE);
@@ -82,29 +125,14 @@ public class MainActivity extends AppCompatActivity {
 
                   // Update list Device
                   DevicesAdapter adapter = new DevicesAdapter();
-                  adapter.btnUpdate = btnUpdate;
                   rvDevices.setAdapter(adapter);
-                  rvDevices.setOnFlingListener(new RecyclerView.OnFlingListener() {
-                        private static final int SWIPE_VELOCITY_THRESHOLD = 4000;
-
-                        @Override
-                        public boolean onFling(int velocityX, int velocityY) {
-                              if (btnUpdate.isEnabled()) {
-                                    if (velocityY < (-1) * SWIPE_VELOCITY_THRESHOLD) {
-                                          update();
-                                          return true;
-                                    }
-                              }
-                              return false;
-                        }
-                  });
                   rvDevices.setLayoutManager(new LinearLayoutManager(this));
             } else {
                   rvDevices.setVisibility(View.INVISIBLE);
                   notFoundFace.setVisibility(View.VISIBLE);
                   txtItemCount.setText(R.string.default_title_no_item_found);
 
-                  Toast.makeText(this, R.string.default_title_no_item_found, Toast.LENGTH_SHORT).show();
+                  Toast.makeText(this, R.string.default_title_no_item_found, Toast.LENGTH_LONG).show();
             }
       }
 }
